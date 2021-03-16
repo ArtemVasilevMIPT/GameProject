6d41fd264487ad46d1b3516a042f5c3c6b8e685d
ardoooo <naumov.av@phystech.edu>
2021-03-17 00:06:03 +0300

    Added inlude/Entities.h and src/Entities.cpp

---
+++ include/Entities.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "core.h"
+#include <cmath>
+#include <list>
+#include <queue>
+
+class Unit : public Object {
+public:
+
+    std::string name;
+    int hp;
+    std::pair<float, float> coordinaties;
+    std::string currentCommant;
+    std::list<std::pair<float, float>> currentPath;
+
+    Unit() = default;
+    ~Unit() = default;
+
+    void move(std::pair<float, float> destPoint);
+    void shoot(std::pair<float, float> destPoint);
+    void destroy();    
+};
+
+class Building : public Object {
+public:
+    
+    std::string name;
+    int hp;
+
+    Building() = default;
+    ~Building() = default;
+
+
+    void Destroy();
+};
+
+class HQ : public Building {
+public: 
+    HQ() = default;
+    ~HQ() = default;
+};
+
+class Factory : public Building {
+public:
+
+    std::unordered_map<std::string, Unit> units;
+    std::queue<std::string> buildQueue;
+    std::pair<float, float> rallyPoint;
+
+    Factory() = default;
+    ~Factory() = default;
+
+    void build();
+}
