919b9e0b8cda794aba1655453e5e064e3ccca2c3
Vasilev Artem <vasilev.artem@phystech.edu>
2021-04-02 12:39:15 +0300

    Deleted commented code

--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -1,6 +1,5 @@
 #include "pathfinding.h"
 #include <cmath>
-#include <iostream>
 
 std::queue<std::pair<float, float>> NavComponent::findPath(std::pair<float, float> start, std::pair<float, float> finish, float speed) {
     float xst = start.first;
@@ -27,35 +26,8 @@ std::queue<std::pair<float, float>> NavComponent::findPath(std::pair<float, floa
             continue;
         } else {
             break;
-            /*int hflag = 0, vflag = 0;
-            if (map[x_next][y_next + 1] || map[x_next][y_next - 1]) {
-                if (xend >= xst) {
-                    hflag = 1;
-                } else {
-                    hflag = -1;
-                }
-            }
-            if (map[x_next + 1][y_next] || map[x_next - 1][y_next]) {
-                if (yend >= yst) {
-                    vflag = 1;
-                } else {
-                    vflag = -1;
-                }
-            }
-
-            while (map[x_next][y_next])*/
         }
     }
-
-    //std::cout << "start: " << start.first << ' ' << start.second << '\n';
-    //std::cout << "finish: " << finish.first << ' ' << finish.second << '\n';
-    /*
-    std::queue<std::pair<float, float>> path1 = path;
-    for (int i = 0; i < path.size(); ++i) {
-        //std::cout << path1.front().first << ' ' << path1.front().second << '\n';
-        path1.pop();
-    }
-     */
 
     return path;
 }
